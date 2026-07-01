e16ce3dfb2967c48ad5553d52554c84a1cef64e4
Henning <>
2026-07-01 14:09:14 +0200

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -4,26 +4,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/25.11";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-old, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python36;
+        python = pkgs.python3;
         pythonEnv = python.withPackages (ps: with ps; [
+          pip # For freezing
           cryptography
         ]);
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
       in {
-        /*apps.default = {
+        apps.default = {
           type = "app";
-          program = toString (pkgs.writeShellScript "run" "${pythonEnv}/bin/python run.py $@");
-        };*/
+          program = toString (pkgs.writeShellScript "run" "${pythonEnv}/bin/python src/main.py $@");
+        };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [ pythonEnv ];
+          packages = [ pythonEnv ];
           shellHook = ''
             export PS1="[dev] \u@\h:\w$ "
           '';
